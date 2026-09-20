97e01c68cee7c13ea4de99be3c6e63eaed72dd55
Mewnot-jar <martinardiles.a@gmail.com>
2026-09-20 15:57:14 -0300

    cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,7 +7,7 @@ app = FastAPI(title="Observacion Maritima Api")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://observacion-maritima.vercel.app/"],
+    allow_origins=["https://observacion-maritima.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
