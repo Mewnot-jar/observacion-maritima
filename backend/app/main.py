66d9a22603731114e2551c843b16c6b7dda6c8a8
Mewnot-jar <martinardiles.a@gmail.com>
2026-09-19 23:25:05 -0300

    se agrega url de vercel al cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,7 +7,7 @@ app = FastAPI(title="Observacion Maritima API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://observacion-maritima.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
